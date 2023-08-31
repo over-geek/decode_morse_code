aa9b0cc7dd29b53c7240c3e8c5c965c95ad2deca
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-31 20:36:57 +0000

    fix linters errors

--- morsedecoder.rb
+++ morsedecoder.rb
@@ -1,29 +1,29 @@
 MORSE_CODE = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z'
-}
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def decode_char(morse_code)
-    MORSE_CODE[morse_code]
+  MORSE_CODE[morse_code]
 end
 
 def decode_word(morse_code_word)
-    morse_characters = morse_code_word.split
-    decoded_characters = morse_characters.map do |morse_character|
-        decode_char(morse_character)
-    end
-    decoded_characters.join
+  morse_characters = morse_code_word.split
+  decoded_characters = morse_characters.map do |morse_character|
+    decode_char(morse_character)
+  end
+  decoded_characters.join
 end
 
 def decode_sentence(morse_code_sentence)
-    sentences = morse_code_sentence.split(' ')
-    sentences.map { |sentence| decode_word(sentence) }.join(' ')
+  sentences = morse_code_sentence.split
+  sentences.map { |sentence| decode_word(sentence) }.join
 end
 
-message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...";
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message = decode_sentence(message)
 puts decoded_message
