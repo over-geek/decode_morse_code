96671f50f0c0ea94014a451af14b039228bf54fb
kenagy100@gmail.com <kenagy100@gmail.com>
2023-08-31 20:01:55 +0000

    Add decode_char and decode_word methods

--- morsedecoder.rb
+++ morsedecoder.rb
@@ -7,3 +7,15 @@ class MorseDecoder
         '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
         '--..' => 'Z'
     }
+
+    def decode_char(morse_code)
+        MORSE_CODE[morse_code]
+    end
+    
+    def decode_word(morse_code_word)
+        morse_characters = morse_code_word.split
+        decoded_characters = morse_characters.map do |morse_character|
+            decode_char(morse_character)
+        end
+        decode_characters.join
+    end
